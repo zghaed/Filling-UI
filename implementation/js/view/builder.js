63f4993e80bdae857c3871373ae0d940a648e76a
Zahra Ghaed <zghaed@gmail.com>
2017-04-26 13:54:07 -0700

    Added customized theme and create four Views

--- implementation/js/view/builder.js
+++ implementation/js/view/builder.js
@@ -1,12 +1,20 @@
 ;(function(app){
 	app.view('Builder', {
 		template: [
-			'<div class="area" region="top_left_box"><button type="button" class="btn btn-success">Test</button></div>',
-			'<div class="area" region="top_right_box">2</div>',
-			'<div class="area" region="middle_box"><h1>HEADER</h1></div>',
-			'<div class="area" region="bottom_right_box">4</div>',
+			'<div class="area" region="top-left-box"></div>',
+			'<div class="area" region="top-right-box"></div>',
+			'<div class="area" region="middle-box"></div>',
+			'<div class="area" region="bottom-right-box"></div>',
 		],
-
+		data: 'boxes.json',
+		// onCertainEvents: function(payload){
+		// 	//
+		// 	//on boxes.json changed
+		// 	this.refresh();
+		//
+		// 	//0000
+		// 	this.set(newBoxes);
+		// },
 		onReady: function(){
 			this.$el.css({
 				'padding': '1em',
@@ -14,50 +22,50 @@
 			  'flex-flow': 'row wrap',
 			  'justify-content': 'space-between',
 			});
-			this.$el.find('.area').css({
-				'padding': '1em',
-				'flex': '0 1 100%',
-				'display': 'flex',
-				'border-width': '2px',
-			  'border-color': 'lightgrey',
-			  'border-style': 'dashed'
-			});
-			this.$el.find('[region="top_left_box"]').css({
-				'flex': '0 1 auto',
-			  'flex-flow': 'row nowrap',
-			  'order': '1',
-			  'justify-content': 'flex-start',
-			  'align-items': 'flex-start',
-			  'align-content': 'flex-start',
-			  'border-bottom-style': 'none'
-			});
-			this.$el.find('[region="top_right_box"]').css({
-				'flex': '0 1 auto',
-			  'flex-flow': 'row-reverse nowrap',
-			  'order': '2',
-			  'justify-content': 'flex-start',
-			  'align-items': 'flex-start',
-			  'align-content': 'flex-start',
-			  'border-bottom-style': 'none'
-			});
-			this.$el.find('[region="middle_box"]').css({
-			  'flex-flow': 'column nowrap',
-			  'order': '3',
-			  'justify-content': 'flex-start',
-			  'align-content': 'flex-start'
-			});
-			this.$el.find('[region="bottom_right_box"]').css({
-				'border-top-style': 'none',
-			  'flex': '0 1 auto',
-			  'order': '4',
-			  'float': 'right',
-			  'margin-left': 'auto',
-			  'flex-flow': 'row-reverse nowrap',
-			  'justify-content': 'flex-start',
-			  'align-items': 'flex-end',
-			  'align-content': 'flex-start'
-			});
+			this.show('middle-box', new Box());//, options);
 		}
+	});
+
+	var Box = app.view({
+		name: 'box',
+		template: [
+			'<div class="wrapper-full">',
+				'<div style="color:#626262;">Boxes</div>',
+				'<div region="group"></div>',
+			'</div>',
+		],
+		useParentData: 'boxes',
+		onReady: function() {
+			console.log(this.get('boxes'));
+			this.more('group', this.get('boxes'), Group, true);
+		}
+	});
+
+	var Group = app.view('Group', {
+		template: [
+			'<div region="info"></div>',
+			'<div region="add"></div>',
+		],
+		useParentData: 'boxes',
+		onReady: function() {
+			this.show('info', Info, {
+				data: this.get()
+			});
+			this.show('add', AddButton);
+		},
+		actions: {
+			'add-element': function(){
+				 app.notify('Action triggered!', 'Click action!', 'success');
+			}
+		}
+	});
+
+	var Info = app.view({
+			template: '<div>{{id}}</div><div>{{name}}</div>',
+	});
+
+	var AddButton = app.view({
+		template: '<div class="add-element" action-click="add-element" data-placement="bottom">Add</div>'
 	});
 
 })(Application);
