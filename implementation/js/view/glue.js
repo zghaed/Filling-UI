4239772d92b4aa237e6de2b1e50e5b595476bf45
Zahra Ghaed <zghaed@gmail.com>
2017-05-03 17:07:48 -0700

    Adding preview when refreshing the page

--- implementation/js/view/glue.js
+++ implementation/js/view/glue.js
@@ -5,28 +5,82 @@
 		onNavigateTo: function(path){
 			var view = app.get(path).create();
 			this.show('glue-region', view);
+			this.listenTo(view, 'ready', function() {
+				this.onReady();
+			});
 		},
 		actions: {
-			'show-builder': function() {
-				var target = $(event.target),
-					cacheName = this.getViewIn('glue-region').name + '-' + target.attr('region');
-				var builder = app.get('Builder').create({
-					data: {
-						"name" : cacheName
- 					}
+			'show-builder': function($btn, e){
+				var target = $(e.target),
+					cacheName = this.getViewIn('glue-region').name + '-' + target.attr('region'),
+					regions = (target.attr('region') === undefined) ||
+						(target.attr('region') === ('middle-box' || 'top-left-box' || 'top-right-box' || 'bottom-right-box'));
+				var allCaches = app.store.getAll();
+				if (!(allCaches[cacheName] || regions)) {
+					var builder = app.get('Builder').create({
+						data: {
+							"name" : cacheName
+	 					}
+					});
+					app.store.set(cacheName, app.store.get(cacheName) || {
+						boxes:
+						[
+							{"template": "", "data":"", "direction":"h", "boxName":"top-left-box",     "groupNumber":0},
+							{"template": "", "data":"", "direction":"h", "boxName":"top-right-box",    "groupNumber":0},
+							{"template": "", "data":"", "direction":"v", "boxName":"middle-box",       "groupNumber":0},
+							{"template": "", "data":"", "direction":"h", "boxName":"bottom-right-box", "groupNumber":0}
+						]
+					});
+					app.spray(target, builder);
+				}
+			}
+		},
+		onReady: function() {
+			if (this.getViewIn('glue-region')) {
+				var caches = app.store.getAll();
+				var keys = [];
+				var viewName = this.getViewIn('glue-region').name;
+				_.each(caches, function(val, key){
+					var keyArray = key.split('-');
+					if (keyArray[0] === viewName) {
+				    keys.push(key);
+				  }
 				});
-				app.store.set(cacheName, app.store.get(cacheName) || {
-					boxes:
-					[
-						{"template": "", "data":"", "direction":"h", "boxName":"top-left-box",     "groupNumber":0},
-						{"template": "", "data":"", "direction":"h", "boxName":"top-right-box",    "groupNumber":0},
-						{"template": "", "data":"", "direction":"v", "boxName":"middle-box",       "groupNumber":0},
-						{"template": "", "data":"", "direction":"h", "boxName":"bottom-right-box", "groupNumber":0}
-					]
+				console.log('existed = ', keys);
+				var self = this;
+				_.each(keys, function(key){
+					console.log('key is, ', key);
+					var builder = app.get('Builder').create({
+						data: {
+							"name" : key
+	 					}
+					});
+					app.store.set(key, app.store.get(key) || {
+						boxes:
+						[
+							{"template": "", "data":"", "direction":"h", "boxName":"top-left-box",     "groupNumber":0},
+							{"template": "", "data":"", "direction":"h", "boxName":"top-right-box",    "groupNumber":0},
+							{"template": "", "data":"", "direction":"v", "boxName":"middle-box",       "groupNumber":0},
+							{"template": "", "data":"", "direction":"h", "boxName":"bottom-right-box", "groupNumber":0}
+						]
+					});
+					var nameArray = key.split('-');
+					nameArray.shift();
+					var name = nameArray.join('-'),
+						sprayResult = app.spray($('[region="'+name+'"]'), builder);
+					sprayResult.$el.attr('region', name);
+					self.listenTo(builder, 'ready', function() {
+						console.log('in listen to, ', self.$el.find('[region="'+name+'"] .region-add'));
+						console.log('this works, ', self.$el.find('[region="'+name+'"] .triangle-top-right-box'));
+						self.$el.find('[region="'+name+'"] .add-button').toggleClass('toggle-preview');
+				    self.$el.find('[region="'+name+'"] .direction').toggleClass('toggle-preview');
+						self.$el.find('[region="'+name+'"] .triangle-top-left-box').toggleClass('toggle-preview');
+						self.$el.find('[region="'+name+'"] .triangle-top-right-box').toggleClass('toggle-preview');
+						self.$el.find('[region="'+name+'"] .triangle-bottom-right-box').toggleClass('toggle-preview');
+				    self.$el.find('[region="'+name+'"] .area').toggleClass('toggle-borders');
+					});
 				});
-				app.spray(target, builder);
 			}
 		}
 	});
-
 })(Application);
