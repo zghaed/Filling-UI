709038a369acc666cf944214704318c43cd41359
Zahra Ghaed <zghaed@gmail.com>
2017-05-06 14:36:23 -0700

    Changed test views to onRender instead of onReady

--- implementation/js/view/test-b.js
+++ implementation/js/view/test-b.js
@@ -1,19 +1,19 @@
 ;(function(app){
 
-	app.view('TestB', {
-		name: 'TestB',
-		template: [
-			'<div class="col-md-12 wrapper-full" style="height:75em;"><div region="test-b" style="border: 1px solid black;"></div></div>',
-			],
-		onReady: function(){
-			this.$el.find('[region="test-b"]').flexlayout([
-					['1:region1:', ['1:region-1-1:']],
-					['1:region2:', ['1:region-2-1:']],
-				], {
-				adjust: true,
-				bars: { flex: '0 0 3px', 'background-color': '#CCC'}
-			});
-		}
-	});
+  app.view('TestB', {
+    name: 'TestB',
+    template: [
+      '<div class="col-md-12 wrapper-full" style="height:75em;"><div region="test-b" style="border: 1px solid black;"></div></div>',
+      ],
+    onRender: function(){
+      this.$el.find('[region="test-b"]').flexlayout([
+          ['1:region1:', ['1:region-1-1:']],
+          ['1:region2:', ['1:region-2-1:']],
+        ], {
+        adjust: true,
+        bars: { flex: '0 0 3px', 'background-color': '#CCC'}
+      });
+    }
+  });
 
 })(Application);
